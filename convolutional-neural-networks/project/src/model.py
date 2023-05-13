Fix train/val accuracy calculation; Reduce number of convolution layers; Save model weights for each HPO round

convolutional-neural-networks/project/src/model.py
@@ -17,69 +17,47 @@ class MyModel(nn.Module):
         #  Input size: 224x224        
         #  Feature map channel output size = (input_size - kernel_size + 2*padding)  + 1
 
-        # Alexnet - https://github.com/pytorch/vision/blob/main/torchvision/models/alexnet.py        
-         # VGG-16 - https://gist.github.com/KushajveerSingh/7773052dfb6d8adedc53d0544dedaf60    
-         #  https://towardsdatascience.com/what-your-validation-loss-is-lower-than-your-training-loss-this-is-why-5e92e0b1747e
         self.model = nn.Sequential(
             nn.Conv2d(3, 64, 3, padding=1),       # output size: (224 - 3 + 2*1) + 1 = 224            
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),                            # output size: (224/2) = 112
-            
+            nn.MaxPool2d(2, 2),                            # output size: (224/2) = 112            
             # nn.Dropout2d(p=dropout),
 
             nn.Conv2d(64, 128, 3, padding=1),     # output size: (112 - 3 + 2*1) + 1 = 112           
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(128, 128, 3, padding=1),     # output size: (112 - 3 + 2*1) + 1 = 112           
-            nn.BatchNorm2d(128),
-            nn.ReLU(),            
-            nn.MaxPool2d(2, 2),                            # output size: (112/2) = 56
-
-            
+            nn.MaxPool2d(2, 2),                            # output size: (112/2) = 56            
             # nn.Dropout2d(p=dropout),
 
             nn.Conv2d(128, 192, 3, padding=1),    # output size: (56 - 3 + 2*1) + 1 = 56
             nn.BatchNorm2d(192),            
             nn.ReLU(),
-            nn.Conv2d(192, 192, 3, padding=1),     # output size: (56 - 3 + 2*1) + 1 = 56           
-            nn.BatchNorm2d(192),            
-            nn.ReLU(),
             nn.MaxPool2d(2, 2),                           # output size: (56/2) = 28
-            
-            
             # nn.Dropout2d(p=dropout),
             
             nn.Conv2d(192, 256, 3, padding=1),   # output size: (28 - 3 + 2 *1) + 1 = 28
             nn.BatchNorm2d(256),   
             nn.ReLU(),
-            nn.Conv2d(256, 256, 3, padding=1),   # output size: (28 - 3 + 2 *1) + 1 = 28         
-            nn.BatchNorm2d(256),   
-            nn.ReLU(),            
             nn.MaxPool2d(2, 2),                            # output size: 14x14
             # nn.Dropout2d(dropout),
 
             nn.Conv2d(256, 384, 3, padding=1),   # output size: (14 - 3 + 2 *1) + 1 = 14
             nn.BatchNorm2d(384),   
             nn.ReLU(),
-            nn.Conv2d(384, 384, 3, padding=1),   # output size: (14 - 3 + 2 *1) + 1 = 14
-            nn.BatchNorm2d(384),   
-            nn.ReLU(),            
             nn.MaxPool2d(2, 2),                            # output size: 7x7
 
             # Flatten feature maps
             nn.Flatten(),            
-            nn.Linear(7 * 7 * 384, 4096),             
-            nn.Dropout(p=dropout),
-            nn.BatchNorm2d(4096), 
+            nn.Linear(7 * 7 * 384, 4096),
+            # Batch normalization not called to avoid 'Expected more than 1 value per channel when training, got input size torch.Size([1, 4096])' errors on small batch sizes
+            # nn.BatchNorm1d(4096), 
+            nn.Dropout(p=dropout),            
             nn.ReLU(),
                                     
             nn.Linear(4096, 4096),
-            nn.Dropout(p=dropout),
-            nn.BatchNorm2d(4096),
+            # nn.BatchNorm1d(4096),
+            nn.Dropout(p=dropout),            
             nn.ReLU(),
                         
             nn.Linear(4096, num_classes)            

convolutional-neural-networks/project/src/train.py
@@ -27,7 +27,7 @@ def train_one_epoch(train_dataloader, model, optimizer, loss):
     train_loss = 0.0
     
     train_correct = 0
-    num_train_samples = len(train_dataloader.dataset)
+    train_total = 0
 
     for batch_idx, (data, target) in tqdm(
         enumerate(train_dataloader),
@@ -61,14 +61,23 @@ def train_one_epoch(train_dataloader, model, optimizer, loss):
         )
         
         # Get class predictions from the outputs
-        _, preds = torch.max(output.data, 1)
+        # _, preds = torch.max(output.data, 1)
+
+        # Update the number of correct predictions from the epoch
+        # train_correct += (preds == target).sum().item()
+        
+        # convert logits to predicted class
+        # HINT: the predicted class is the index of the max of the logits
+        pred  = torch.argmax(output, dim=1) # YOUR CODE HERE
         
         # Update the number of correct predictions from the epoch
-        train_correct += (preds == target).sum().item()
+        train_correct += torch.sum(torch.squeeze(pred.eq(target.data.view_as(pred))).cpu())
+        
+        train_total += data.size(0)
         
     print(
         'Training Accuracy: {:.2f}%'.format(
-            100 * train_correct/num_train_samples,
+            100 * train_correct/train_total,
         )
     )
         
@@ -79,10 +88,7 @@ def train_one_epoch(train_dataloader, model, optimizer, loss):
 def valid_one_epoch(valid_dataloader, model, loss):
     """
     Validate at the end of one epoch
-    """
-
-    num_val_samples = len(valid_dataloader.dataset)
-    val_correct = 0
+    """    
     
     with torch.no_grad():
 
@@ -94,6 +100,9 @@ def valid_one_epoch(valid_dataloader, model, loss):
             model.cuda()
 
         valid_loss = 0.0
+        val_correct = 0
+        val_total = 0
+        
         for batch_idx, (data, target) in tqdm(
             enumerate(valid_dataloader),
             desc="Validating",
@@ -115,13 +124,18 @@ def valid_one_epoch(valid_dataloader, model, loss):
                 (1 / (batch_idx + 1)) * (loss_value.data.item() - valid_loss)
             )
             
-            _, preds = torch.max(output.data, 1)
-            
-            val_correct += (preds == target).sum().item()
+            # convert logits to predicted class
+            # HINT: the predicted class is the index of the max of the logits
+            pred  = torch.argmax(output, dim=1) # YOUR CODE HERE
+
+            # Update the number of correct predictions from the epoch
+            val_correct += torch.sum(torch.squeeze(pred.eq(target.data.view_as(pred))).cpu())
+
+            val_total += data.size(0)
             
         print(
             'Validation Accuracy: {:.2f}%'.format(
-                100 * val_correct/num_val_samples,
+                100 * val_correct/val_total,
             )
         )            
 
@@ -155,7 +169,7 @@ def optimize(data_loaders, model, optimizer, loss, n_epochs, save_path, interact
 
         # print training/validation statistics
         print(
-            "Epoch: {} \tTraining Loss2: {:.6f} \tValidation Loss2: {:.6f}".format(
+            "Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}".format(
                 epoch, train_loss, valid_loss
             )
         )